d629f4cbb9776ce0e641af6390c209c1ae8ed297
Milad Azami <miladazami120@gmail.com>
2024-01-13 18:04:15 +0330

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
+import TodoApp from "./TodoApp";
+import { AbilityContext } from "./Can";
+import defineAbilityFor from "./ability";
+
 import "./App.css";
+import PermissionController from "./PermissionController";
 
 function App() {
+  // list of permissions
+  const [permissions, setPermissions] = useState({
+    Todo: ["read", "update", "delete", "create"],
+  });
+
+  // ability instance
+  const ability = defineAbilityFor(permissions);
+
   return (
     <>
-      <h1>Botostart</h1>
+      <PermissionController
+        permissions={permissions}
+        setPermissions={setPermissions}
+      />
+      <AbilityContext.Provider value={ability}>
+        <TodoApp />
+      </AbilityContext.Provider>
     </>
   );
 }
